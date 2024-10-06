8ddb3fa255dce94e5e60fd9172a57e1de1510864
KewbitXMR <kewbitxmr@protonmail.com>
2024-10-07 00:03:43 +0100

    added new getAutoswitch feature

--- lib/src/grpc/grpc.pbgrpc.dart
+++ lib/src/grpc/grpc.pbgrpc.dart
@@ -1,8 +1,23 @@
+// Haveno Flutter extends the features of Haveno, supporting mobile devices and more.
+// Copyright (C) 2024 Kewbit (https://kewbit.org)
+// Source Code: https://git.haveno.com/haveno/flutter_haveno.git
 //
-//  Generated code. Do not modify.
-//  source: grpc.proto
+// Author: Kewbit
+//    Website: https://kewbit.org
+//    Contact Email: kewbitxmr@protonmail.com or me@kewbit.org
 //
-// @dart = 2.12
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // ignore_for_file: annotate_overrides, camel_case_types, comment_references
 // ignore_for_file: constant_identifier_names, library_prefixes
